4d2d62447ec54b9f73d7a7dd7de58d66ac043cba
Gaurav Rai Bhatt <gaurav.rai.bhatt@gmail.com>
2023-05-11 00:30:02 +0530

    done

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "cd738a7ee008fc1fc226379b9feeab5e",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
